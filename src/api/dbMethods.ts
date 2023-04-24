f921596603de42ebf42aacc19d5dd41043fc941c
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-04-25 01:41:07 +0400

    added check if user already exists in db

--- src/api/dbMethods.ts
+++ src/api/dbMethods.ts
@@ -1,13 +1,20 @@
 import { User } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import { db } from "./api";
 
 export const saveUser = async (user: User) => {
   const { uid, displayName, photoURL, email } = user;
 
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return;
+  }
+
   try {
-    await setDoc(doc(db, "users", uid), { uid, displayName, photoURL, email });
+    await setDoc(docRef, { uid, displayName, photoURL, email });
   } catch (err: any) {
     toast.error(err.mesage);
   }
