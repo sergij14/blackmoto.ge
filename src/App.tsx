3e690a9b061c1df4635ac7a771d42622c0e8042d
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-04-24 17:37:14 +0400

    added assets/setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./styles/index.css";
 
 const App = () => {
-  return <div>MotoRent</div>;
+  return (
+    <div className="px-4 max-w-screen-xl mx-auto">
+      <div className="flex flex-col items-center gap-2 justify-center py-12">
+        <h2 className="text-3xl font-black">
+          <a href="/">#Moto Rent</a>
+        </h2>
+        <img className="max-w-5xl" alt="" src="/assets/hero.svg" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
