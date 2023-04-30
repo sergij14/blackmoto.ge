000f6d4ffe0dd771f86658cbaf5f574b04e43357
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-04-30 13:21:59 +0400

    extracted header component

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -5,21 +5,11 @@ import { auth } from "../api/api";
 import { ItemWithId } from "../types";
 import { createCollection, saveUser } from "../api/dbMethods";
 import { onSnapshot, query } from "firebase/firestore";
-import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
-import { userSignIn, userSignOut } from "../api/authMethods";
-import {
-  ArrowLeftOnRectangleIcon,
-  ArrowRightOnRectangleIcon,
-  ChatBubbleOvalLeftIcon,
-  HomeIcon,
-  UserCircleIcon,
-} from "@heroicons/react/24/solid";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Header from "./Header";
 
 const AppWrapper = () => {
-  const { setUser, setItems, user } = useStore();
-
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
+  const { setUser, setItems } = useStore();
 
   useEffect(() => {
     const unsubAuthState = onAuthStateChanged(auth, (user) => {
@@ -51,45 +41,7 @@ const AppWrapper = () => {
         alt=""
         src="/rent-moto/assets/hero.svg"
       />
-      <header className="flex flex-col gap-4 items-center py-8 md:flex-row md:justify-between">
-        <h2 className="text-3xl md:text-4xl font-black">
-          <Link to="/">#Moto Rent</Link>
-        </h2>
-        <nav className="flex gap-2 flex-col items-center justify-center sm:flex-row flex-wrap">
-          <button className="nav-btn" onClick={() => navigate("/")}>
-            <HomeIcon />
-            Home
-          </button>
-          <button className="nav-btn" onClick={() => navigate("/admin")}>
-            <UserCircleIcon />
-            Admin
-          </button>
-          <button className="nav-btn">
-            <ChatBubbleOvalLeftIcon /> WhatsApp
-          </button>
-        </nav>
-      </header>
-      {pathname.slice(1) === "admin" && (
-        <>
-          {user ? (
-            <div className="flex flex-col gap-4 items-center pb-8 md:flex-row md:justify-between">
-              <div className="flex gap-2 items-center">
-                {user.photoURL && (
-                  <img src={user.photoURL} className="w-8 rounded-full" />
-                )}
-                {user.email}
-              </div>
-              <button className="nav-btn" onClick={() => userSignOut(navigate)}>
-                <ArrowLeftOnRectangleIcon /> Sign Out
-              </button>
-            </div>
-          ) : (
-            <button className="nav-btn" onClick={userSignIn}>
-              <ArrowRightOnRectangleIcon /> Sign In
-            </button>
-          )}
-        </>
-      )}
+      <Header />
       <Outlet />
       <footer className="bg-gray-900 bg-opacity-20 mt-14 mb-8 rounded-md p-10 text-center">
         &copy; {new Date().getFullYear()}, All rights reserved
---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { useStore } from "../store";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { userSignIn, userSignOut } from "../api/authMethods";
+import {
+  ArrowLeftOnRectangleIcon,
+  ArrowRightOnRectangleIcon,
+  ChatBubbleOvalLeftIcon,
+  HomeIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { user } = useStore();
+
+  return (
+    <>
+      <header className="flex flex-col gap-4 items-center py-8 md:flex-row md:justify-between">
+        <h2 className="text-3xl md:text-4xl font-black">
+          <Link to="/">#Moto Rent</Link>
+        </h2>
+        <nav className="flex gap-2 flex-col items-center justify-center sm:flex-row flex-wrap">
+          <button className="nav-btn" onClick={() => navigate("/")}>
+            <HomeIcon />
+            Home
+          </button>
+          <button className="nav-btn" onClick={() => navigate("/admin")}>
+            <UserCircleIcon />
+            Admin
+          </button>
+          <button className="nav-btn">
+            <ChatBubbleOvalLeftIcon /> WhatsApp
+          </button>
+        </nav>
+      </header>
+      {pathname.slice(1) === "admin" && (
+        <>
+          {user ? (
+            <div className="flex flex-col gap-4 items-center pb-8 md:flex-row md:justify-between">
+              <div className="flex gap-2 items-center">
+                {user.photoURL && (
+                  <img src={user.photoURL} className="w-8 rounded-full" />
+                )}
+                {user.email}
+              </div>
+              <button className="nav-btn" onClick={() => userSignOut(navigate)}>
+                <ArrowLeftOnRectangleIcon /> Sign Out
+              </button>
+            </div>
+          ) : (
+            <button className="nav-btn" onClick={userSignIn}>
+              <ArrowRightOnRectangleIcon /> Sign In
+            </button>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Header;
