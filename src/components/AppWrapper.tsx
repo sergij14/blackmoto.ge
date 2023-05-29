64e564734bab63d387c78e105bcff0f9506e0468
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-29 10:51:44 +0400

    saving specific authed users to DB

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,18 +1,27 @@
 import React, { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { toast } from "react-toastify";
+import { Outlet } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useStore } from "../store";
 import { auth } from "../api/api";
 import { ItemWithId } from "../types";
 import { createCollection, saveUser } from "../api/dbMethods";
 import { onSnapshot, query } from "firebase/firestore";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "./Header";
+
+const AUTHED_EMAILS = (import.meta.env.VITE_ADMIN_EMAILS as string).split(",");
 
 const AppWrapper = () => {
   const { setUser, setItems } = useStore();
 
   useEffect(() => {
     const unsubAuthState = onAuthStateChanged(auth, (user) => {
+      const userEmail = user?.email;
+      if (userEmail && !AUTHED_EMAILS.includes(userEmail)) {
+        signOut(auth);
+        toast.error("You have no access");
+        return;
+      }
       if (user) {
         setUser(user);
         saveUser(user);
