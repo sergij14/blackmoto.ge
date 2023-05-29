0417d9b60e55b8900b258e2fac47dc11781a2518
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-29 11:01:10 +0400

    Updates

--- assets/Admin-bce354b8.js
+++
@@ -1,1 +0,0 @@
-import{b as t,c as r,F as e,j as o}from"./index-610a94d9.js";import{I as i}from"./ItemForm-5797eeb3.js";import"./dbMethods-eddf5a41.js";import"./ItemCmp-60745405.js";const d=()=>{const{user:s}=t();return r(e,{children:s&&o("div",{children:[r("h4",{className:"section-title",children:"Add Moto"}),r(i,{})]})})};export{d as default};
