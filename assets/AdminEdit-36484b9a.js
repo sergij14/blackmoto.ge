ffc62b071c5f280ff7995852cf33e6815b2e0b87
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-05-29 11:36:06 +0400

    Updates

---
+++ assets/AdminEdit-36484b9a.js
@@ -0,0 +1,1 @@
+import{d as i,j as o,c as t}from"./index-ab32eabe.js";import{I as r}from"./ItemForm-d340b44d.js";import"./dbMethods-7e8ae2ca.js";import"./ItemCmp-72969cf5.js";const c=()=>{const{id:s}=i();return o("div",{children:[t("h4",{className:"section-title",children:"Edit Moto"}),t(r,{itemId:s})]})};export{c as default};
