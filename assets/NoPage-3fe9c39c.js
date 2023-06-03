8f5110f53184b0e069ff8b7496f79ae0baa83825
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-06-04 01:16:01 +0400

    Updates

---
+++ assets/NoPage-3fe9c39c.js
@@ -0,0 +1,1 @@
+import{b as s,r as o,j as e}from"./index-c72ca1d2.js";const c=()=>{const t=s();return o.useEffect(()=>{const a=setTimeout(()=>{t("/")},3e3);return()=>{clearTimeout(a)}},[]),e("div",{children:e("h4",{className:"section-title my-14 text-center",children:"404 Page Not Found"})})};export{c as default};
